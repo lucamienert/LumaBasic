b8ce527aa5c8b9a53319c3f63a4b01c6a4be10b5
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-05-03 12:20:34 +0200

    Updating Parser and adding IO

---
+++ src/include/io.h
@@ -0,0 +1,11 @@
+#ifndef LUMALANG_IO_H_
+#define LUMALANG_IO_H_
+
+#pragma once
+
+#include <stdlib.h>
+#include <string.h>
+
+char *get_file(char *filename);
+
+#endif
--- src/include/parser.h
+++ src/include/parser.h
@@ -3,23 +3,34 @@
 
 #pragma once
 
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "token.h"
 #include "tokenizer.h"
 #include "ast.h"
+#include "scope.h"
+#include "misc.h"
 
-typedef struct Parser
+typedef struct PARSER_STRUCT
 {
     tokenizer_t *tokenizer;
-    token_t *token;
+    token_t *current_token;
+    token_t *previous_token;
+    scope_t *scope;
 } parser_t;
 
-parser_t *init_parser(tokenizer_t *tokenizer);
+parser_t *new_parser(tokenizer_t *tokenizer);
 
-token_t *eat(parser_t *parser, int type);
+void eat(parser_t *parser, int token_type);
 
-
+ast_t *parse(parser_t *parser, scope_t *scope);
+ast_t *parse_statement(parser_t *parser, scope_t *scope);
+ast_t *parse_statements(parser_t *parser, scope_t *scope);
+ast_t *parse_expression(parser_t *parser, scope_t *scope);
+ast_t *parse_factor(parser_t *parser, scope_t *scope);
+ast_t *parse_term(parser_t *parser, scope_t *scope);
+ast_t *parse_function_call(parser_t *parser, scope_t *scope);
+ast_t *parse_variable_definition(parser_t *parser, scope_t *scope);
+ast_t *parse_function_definition(parser_t *parser, scope_t *scope);
+ast_t *parse_variable(parser_t *parser, scope_t *scope);
+ast_t *parse_string(parser_t *parser, scope_t *scope);
+ast_t *parse_id(parser_t *parser, scope_t *scope);
 
 #endif

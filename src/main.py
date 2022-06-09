bdda06325caad9f75763b87f7c55a27a7518155e
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-06-09 15:30:43 +0200

    Changed to file reading

--- src/main.py
+++ src/main.py
@@ -1,9 +1,26 @@
-from .interpreter import Interpreter
+from interpreter import Interpreter
+from parser import Parser
+from tokenizer import Tokenizer
 
-def main(filename):
-    if filename.endswith('.lbas'):
+import sys
+
+def main():
+    filename = sys.argv[1]
+    if filename.endswith('.lmath'):
         with open(filename, 'r') as file:
-            data = file.read().splitlines()
+            data = file.read()
+
+    tokenizer = Tokenizer(data)
+    tokens = tokenizer.generate_tokens()
+
+    parser = Parser(tokens)
+    ast = parser.parse()
+    if not ast:
+        raise Exception("Not AST!")
 
     interpreter = Interpreter()
-    interpreter.interpret(data)
+    value = interpreter.visit(ast)
+    print(f"[MATH]: {value}")
+
+if __name__ == '__main__':
+    main()
